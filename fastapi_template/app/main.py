bd9163d210378d3ef3d808c583cb1816c3c5982d
Vigrel <eller.vini@gmail.com>
2024-08-18 15:09:25 -0300

    feat(api): add query route example

--- fastapi_template/app/main.py
+++ fastapi_template/app/main.py
@@ -22,5 +22,25 @@ def predict(X: str = Query(..., description="Input text for prediction")):
     result = app.predictor.predict(X)
     return {"input_value": X, "predicted_value": result, "message": "prediction successful"}
 
+@app.get("/query")
+def query_route(query: str = Query(..., description="Search query")):
+    # TODO: write your code here, keeping the return format
+    return {"results": [   {'title':'Document title',
+        'content':'Document content (perhaps only the first 500 words?',
+        'relevance': 0.3
+        },
+        {'title':'Document title',
+        'content':'Document content (perhaps only the first 500 words?',
+        'relevance': 0.2
+        },
+        {'title':'Document title',
+        'content':'Document content (perhaps only the first 500 words?',
+        'relevance': 0.1
+        }
+    ], "message": "OK"}
+
 def run():
     uvicorn.run("fastapi_template.main:app", host="0.0.0.0", port=8000, reload=True)
+
+if __name__ == "__main__":
+    run()
